fc58983545ccf8e0fd232c1adb10d86ac130cb68
Anais HACHEMANE <anais.hachemane@essec.edu>
2026-07-13 18:37:02 +0200

    Delete ancient route system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,13 @@ import HeroSection from "../app/component/HeroSection";
 import BestSeller from "./component/BestSeller";
 import Plan from "./component/Plan";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const { section } = await searchParams;
+export default async function Home({}) {
   return (
     <>
       <NavBar />
-      {section === "menu" ? (
-        <></>
-      ) : section === "contact" ? (
-        <></>
-      ) : (
-        <>
-          <HeroSection />
-          <BestSeller />
-          <Plan />
-        </>
-      )}
+      <HeroSection />
+      <BestSeller />
+      <Plan />
     </>
   );
 }
